fab17c81d89b32528f8b0cd70e5aba1930b3d4bf
Kai Shen <kai.shen@mail.utoronto.ca>
2025-02-28 02:40:33 -0500

    Added more flags.

--- test_bpf_helper/test.sh
+++ test_bpf_helper/test.sh
@@ -3,15 +3,26 @@ set -e
 
 # Define kernel source directory
 KERNEL_SRC="/users/kaishen/linux"
+ARCH="x86"
 
-# compile bpf program with proper include paths
+# Get system includes from clang
+SYSTEM_INCLUDES="-I$(clang -print-file-name=include)"
+
+# compile bpf program with comprehensive include paths
 clang -target bpf -O2 -Wall \
+  ${SYSTEM_INCLUDES} \
+  -nostdinc \
   -I${KERNEL_SRC}/include \
   -I${KERNEL_SRC}/include/uapi \
-  -I${KERNEL_SRC}/arch/x86/include \
-  -I${KERNEL_SRC}/arch/x86/include/uapi \
-  -I${KERNEL_SRC}/arch/x86/include/generated/uapi \
   -I${KERNEL_SRC}/include/generated/uapi \
+  -I${KERNEL_SRC}/arch/${ARCH}/include \
+  -I${KERNEL_SRC}/arch/${ARCH}/include/uapi \
+  -I${KERNEL_SRC}/arch/${ARCH}/include/generated \
+  -I${KERNEL_SRC}/arch/${ARCH}/include/generated/uapi \
+  -D__KERNEL__ \
+  -D__BPF_TRACING__ \
+  -Wno-unused-value \
+  -Wno-pointer-sign \
   -c test_bpf.c -o test_bpf.o
 
 # create and pin bpf map (need to be created before user program access)
